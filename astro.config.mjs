061168758143fa9f49392b21105c28ff3153dc0e
ha1ix <gh24@hw.gy>
2024-09-05 08:57:37 -0400

    fixed?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
